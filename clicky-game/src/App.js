30d330c0455ba6d4f05f4ede0586663134e53b49
Alex Sanschagrin <asanscha@gmail.com>
2019-11-02 16:22:09 -0700

    some click functions working

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -2,25 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Click on an image to earn points, but don't click on any more than once!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    colors : [require("./assets/pika.png"), require("./assets/charm.png"), require("./assets/squirtle.png")],
+    clicked: [],
+    score: 0,
+    highscore: -1
+  }
+
+shuffle= (a)=> {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+  }
+  console.log(a)
+  return a;
 }
+
+reset = ()=>{
+  this.setState({score: 0, clicked: []})
+}
+
+checkWL = () =>{
+  if (this.state.score === 3){
+    alert("you win!")
+      this.reset()
+  }
+}
+
+handleClick = (e) =>{
+  let src = e.target.getAttribute("src")
+  console.log(src)
+  if (!this.state.clicked.includes(src)){
+    this.setState({score: this.state.score +1})
+    this.setState({clicked: [...this.state.clicked, src]})
+    this.setState({colors: this.shuffle(this.state.colors)})
+    this.state.score > this.state.highscore? this.setState({highscore: this.state.score}) : console.log(this.state.highscore)
+    setTimeout(this.checkWL, 500)
+  }else{
+    this.setState({score: 0})
+    alert("YOU LOSE")
+    this.reset()
+  }
+  console.log(this.state.clicked)
+}
+
+  render(){
+    return (
+    <div className="App">
+     {this.state.colors.map(color=>{
+       console.log(color)
+       return (<img key={color} onClick={this.handleClick} className="clickables" src={color}/>)
+     })}
+    
+<h1>YOUR CURRENT SCORE: {this.state.score} </h1>
+<h1>YOUR HIGHEST SCORE: {this.state.score >= this.state.highscore ? this.state.score : this.state.highscore + 1}</h1>
+</div>
+  );
+  }};
+
 
 export default App;
